c3be4867776f58ef885d9f8071768abe9a773d72
alex furtado <furtado.vm2@gmail.com>
2025-03-06 16:03:18 -0300

    Added 2 workers

--- start-django.sh
+++ start-django.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export GUNICORN_WORKERS=3 
+export GUNICORN_WORKERS=2 
 python manage.py collectstatic --no-input
 python manage.py migrate
 if [[ "$ENV_STATE" == "production" ]]; then
-    gunicorn --bind 0.0.0.0:8000 myproject.wsgi --workers ${GUNICORN_WORKERS:-3} --forwarded-allow-ips "*"
+    gunicorn --bind 0.0.0.0:8000 myproject.wsgi --workers ${GUNICORN_WORKERS:-2} --forwarded-allow-ips "*"
 else
     python manage.py runserver 0.0.0.0:8000
 fi
